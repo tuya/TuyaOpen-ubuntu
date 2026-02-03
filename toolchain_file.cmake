ded60c289adba3728c6854528e6cb73be2434112
YangJie <shiliu.yang@tuya.com>
2026-02-03 10:07:56 +0800

    fix: improve cross-compilation detection for Raspberry Pi and DshanPi_A1

--- toolchain_file.cmake
+++ toolchain_file.cmake
@@ -5,7 +5,7 @@
 
 include("${CMAKE_CURRENT_LIST_DIR}/compiler_setup.cmake")
 
-if (PLATFORM_BOARD STREQUAL "Raspberry_Pi" AND CONFIG_COMPILE_PREX)
+if (PLATFORM_BOARD STREQUAL "Raspberry_Pi" AND IS_CROSS_COMPILE)
     # Raspberry_Pi build cmake settings
     set(CMAKE_SYSTEM_NAME Linux)
     set(CMAKE_SYSTEM_PROCESSOR aarch64)
